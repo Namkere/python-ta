6e1f1dae36d0c2e598d0f0e05e543c6ba974cd1c
Emediong Namkere <66225577+Namkere@users.noreply.github.com>
2022-06-18 20:36:37 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,6 +4,27 @@ import os
 
 def prime(s):
     # your code goes here
+    prime = True
+
+            for i in range (1, s):
+
+                    if i != 1 :
+
+                        if s % i ==0:
+
+                        	prime = False
+
+                        	
+
+            if prime == True:
+
+            	print(f"{s} is a prime numner")
+
+            else:
+
+                print(f"{s} is not a prime number")
+
+   
 
 def solution(s):
     return prime(s)
